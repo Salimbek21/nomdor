b475bc203bf769beeadd4665536056eca78b8612
SKarimbaev <SKarimbaev@beeline.uz>
2023-06-26 11:51:26 +0500

    ....

---
+++ api/index.js
@@ -0,0 +1,1 @@
+export const url = `https://nomdor-dashboard.onrender.com/api`
--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -2,31 +2,29 @@ import Link from "next/link";
 import cls from "./card.module.scss";
 import Image from "next/image";
 
-export default function Card({ innerData }) {
-	console.log(innerData.data, "card");
-	return (
-		<div className={cls.card_wrapper}>
-			{innerData.data.map((item) => (
-				<Link href={"#"} className={cls.card} key={item.id}>
-					<span className={cls.card_img}>
-						<Image src={item.img} alt="Card img" />
-					</span>
-					<div className={cls.product_body}>
-						<div className={cls.product_texts}>
-							<div className={cls.title}>
-								<span>{item.title}</span>
-							</div>
-							<div className={cls.desc}>
-								<p>{item?.description}</p>
-							</div>
-							<h2 className={cls.price}>{item.price}</h2>
-						</div>
-						<button className={cls.product_btn}>
-							<h4>В корзину</h4>
-						</button>
-					</div>
-				</Link>
-			))}
-		</div>
-	);
+export default function Card(props) {
+
+  return (
+    <div className={cls.card_wrapper}>
+      <Link href={"#"} className={cls.card}>
+        <span className={cls.card_img}>
+          <img src={props.img} alt="Card img" />
+        </span>
+        <div className={cls.product_body}>
+          <div className={cls.product_texts}>
+            <div className={cls.title}>
+              <span>{props.name}</span>
+            </div>
+            <div className={cls.desc}>
+              <p>{props.description}</p>
+            </div>
+            <h2 className={cls.price}>{props.price} so'm</h2>
+          </div>
+          <button className={cls.product_btn}>
+            <h4>В корзину</h4>
+          </button>
+        </div>
+      </Link>
+    </div>
+  );
 }
