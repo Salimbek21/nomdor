a893d9789e436f993a4a72a69fc1ef95c2c1058b
SKarimbaev <SKarimbaev@beeline.uz>
2023-07-02 18:48:58 +0500

    ....

--- components/Product/index.jsx
+++ components/Product/index.jsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import CardLoader from "../CardLoader";
 
 export default function Product({ newData }) {
-<<<<<<< HEAD
   return (
     <>
       <div id="plov" className="container">
@@ -29,27 +28,4 @@ export default function Product({ newData }) {
       </div>
     </>
   );
-=======
-	console.log(newData, "data");
-	return (
-		<>
-			<div className="container">
-				<div className={cls.product_wrapper}>
-					<div className={cls.product_title}>Ош</div>
-					<div className={cls.card_wrapper}>
-						{newData?.length &&
-							newData.map((item) => (
-								<Card
-									name={item.attributes.title}
-									description={item.attributes.description}
-									price={item.attributes.price}
-									img={item?.attributes?.plovImage?.data?.attributes?.url}
-								/>
-							))}
-					</div>
-				</div>
-			</div>
-		</>
-	);
->>>>>>> bbd0808938db7529a2b76c3ab28a68fa39ea91e4
 }
