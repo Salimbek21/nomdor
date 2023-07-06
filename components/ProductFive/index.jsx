bc308d1d0e4ffb908c1f43229773e5c47ab94d80
Miraxtamov <jmiraxtamov@gmail.com>
2023-07-06 15:38:55 +0500

    Some bugs have been fixed

--- components/ProductFive/index.jsx
+++ components/ProductFive/index.jsx
@@ -2,28 +2,26 @@ import Card from "../Card";
 import cls from "../../styles/product.module.scss";
 import CardLoader from "../CardLoader";
 
-export default function ProductFourth({shashdata}) {
-	
-
-	return (
-		<>
-			<div id="salati" className="container">
-				<div className={cls.product_wrapper}>
-					<div className={cls.product_title}>Шашлыки</div>
-					<div className={cls.card_wrapper}>
-						{shashdata?.length &&
-							shashdata?.map((item) => (
-								<Card
-									key={item.id}
-									name={item.attributes.shashlikTitle}
-									description={item.attributes.description}
-									price={item.attributes.price}
-									img={item?.attributes?.shashlikImage?.data?.attributes?.url}
-								/>
-							))}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+export default function ProductFourth({ shashdata }) {
+  return (
+    <>
+      <div id="salati" className="container">
+        <div className={cls.product_wrapper}>
+          <div className={cls.product_title}>Шашлыки</div>
+          <div className={cls.card_wrapper}>
+            {shashdata?.length &&
+              shashdata?.map((item) => (
+                <Card
+                  key={item.id}
+                  name={item.attributes.shashlikTitle}
+                  description={item.attributes.description}
+                  price={item.attributes.price}
+                  img={item?.attributes?.shashlikImage?.data?.attributes?.url}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- components/ProductFourth/index.jsx
+++ components/ProductFourth/index.jsx
@@ -1,28 +1,26 @@
 import Card from "../Card";
 import cls from "../../styles/product.module.scss";
 
-export default function ProductFourth({salatsData}) {
-	
-
-	return (
-		<>
-			<div id="salati" className="container">
-				<div className={cls.product_wrapper}>
-					<div className={cls.product_title}>Салаты</div>
-					<div className={cls.card_wrapper}>
-						{salatsData?.length &&
-							salatsData?.map((item) => (
-								<Card
-									key={item.id}
-									name={item.attributes.salatName}
-									description={item.attributes.description}
-									price={item.attributes.price}
-									img={item?.attributes?.salatImage?.data?.attributes?.url}
-								/>
-							))}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+export default function ProductFourth({ salatsData }) {
+  return (
+    <>
+      <div id="salati" className="container">
+        <div className={cls.product_wrapper}>
+          <div className={cls.product_title}>Салаты</div>
+          <div className={cls.card_wrapper}>
+            {salatsData?.length &&
+              salatsData?.map((item) => (
+                <Card
+                  key={item.id}
+                  name={item.attributes.salatName}
+                  description={item.attributes.description}
+                  price={item.attributes.price}
+                  img={item?.attributes?.salatImage?.data?.attributes?.url}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
